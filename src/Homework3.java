c2d2107700cdd9291dc7e08853d0c2b98d50e058
shter <shterebandrew@gmail.com>
2023-10-01 14:17:20 +0300

    all methods are non-static

--- src/Homework3.java
+++ src/Homework3.java
@@ -3,50 +3,51 @@ import java.util.Scanner;
 public class Homework3 {
 
         public static void main(String[] args) {
+            Homework3 homework = new Homework3();
             //1
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter the first number");
             int first = scanner.nextInt();
             System.out.println("Enter the second number");
             int second = scanner.nextInt();
-            int result = absoluteDifference(first,second);
+            int result = homework.absoluteDifference(first,second);
             System.out.println("Result " + result );
             //2
             Scanner scanner2 = new Scanner(System.in);
             System.out.println("Type any word");
             String word = scanner2.nextLine();
-            char firstChar = getFirstChar(word);
+            char firstChar = homework.getFirstChar(word);
             System.out.println("Result = " + firstChar);
             //3
             Scanner scanner3 = new Scanner(System.in);
             System.out.println("Type any word");
             String word3 = scanner3.nextLine();
-            char lastChar = getLastChar(word3);
+            char lastChar = homework.getLastChar(word3);
             System.out.println("Result = " + lastChar);
             //4
             Scanner scanner4 = new Scanner(System.in);
             System.out.println("Write any word");
             String word4 = scanner4.nextLine();
-            String swappedWord = swapFirstAndLastCharacters(word4);
+            String swappedWord = homework.swapFirstAndLastCharacters(word4);
             System.out.println("Result " + swappedWord);
 
 
         }
 //1
-        public static int absoluteDifference(int first, int second) {
+        public  int absoluteDifference(int first, int second) {
             return Math.abs(first - second);
 //2
         }
-    public static char getFirstChar(String word) {
+    public  char getFirstChar(String word) {
         return word.charAt(0);
     }
 //3
-    public static char getLastChar(String word) {
+    public  char getLastChar(String word) {
         return word.charAt(word.length() - 1);
     }
 
 //4
-    public static String swapFirstAndLastCharacters (String word){
+    public String swapFirstAndLastCharacters (String word){
             return word.charAt(word.length() - 1) + word.substring(1, word.length() - 1) + word.charAt(0);
 
     }
